6011e1a3b510d19203c6d073de3df0d9764610b6
Eugene Pok <zcpok97@gmail.com>
2026-05-24 23:28:32 +0800

    add get method in store

--- internal/store/store.go
+++ internal/store/store.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
@@ -9,20 +10,24 @@ import (
 	"github.com/eugenepok/go-aws-url-shortener/pkg/models"
 )
 
-type PutItemAPI interface {
+// ErrNotFound is returned when no item exists for the given key.
+var ErrNotFound = errors.New("store: url not found")
+
+type DynamoAPI interface {
 	PutItem(ctx context.Context, in *dynamodb.PutItemInput, opts ...func(*dynamodb.Options)) (*dynamodb.PutItemOutput, error)
+	GetItemByKey(ctx context.Context, in *dynamodb.GetItemInput, opts ...func(*dynamodb.Options)) (*dynamodb.GetItemOutput, error)
 }
 
 type DynamoStore struct {
-	client PutItemAPI
+	client DynamoAPI
 	table  string
 }
 
-func New(client PutItemAPI, table string) *DynamoStore {
+func New(client DynamoAPI, table string) *DynamoStore {
 	return &DynamoStore{client: client, table: table}
 }
 
-func (s *DynamoStore) Save(ctx context.Context, m *models.UrlData) error {
+func (s *DynamoStore) AddShortURL(ctx context.Context, m *models.UrlData) error {
 	item, err := attributevalue.MarshalMap(m)
 	if err != nil {
 		return fmt.Errorf("marshal metadata: %w", err)
@@ -35,4 +40,28 @@ func (s *DynamoStore) Save(ctx context.Context, m *models.UrlData) error {
 		return fmt.Errorf("dynamodb put: %w", err)
 	}
 	return nil
+}
+
+func (s *DynamoStore) GetFullURL(ctx context.Context, shortURL string) (*models.UrlData, error) {
+	selectedKey, err := attributevalue.MarshalMap(&models.UrlData{ShortURL: shortURL})
+	if err != nil {
+		return nil, fmt.Errorf("dynamodb get marshal: %w", err)
+	}
+	resp, err := s.client.GetItemByKey(ctx, &dynamodb.GetItemInput{
+		TableName: &s.table,
+		Key:       selectedKey,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("dynamodb get item: %w", err)
+	}
+
+	if resp.Item == nil {
+		return nil, ErrNotFound
+	}
+
+	var item models.UrlData
+	if err := attributevalue.UnmarshalMap(resp.Item, &item); err != nil {
+		return nil, fmt.Errorf("dynamodb get unmarshal: %w", err)
+	}
+	return &item, nil
 }
--- internal/store/store_test.go
+++ internal/store/store_test.go
@@ -6,37 +6,80 @@ import (
 	"testing"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/eugenepok/go-aws-url-shortener/pkg/models"
 	"github.com/stretchr/testify/require"
 )
 
-type fakePutItemAPI struct {
-	gotInput *dynamodb.PutItemInput
-	err      error
+type fakeDynamoAPI struct {
+	gotPutInput  *dynamodb.PutItemInput
+	gotGetInput  *dynamodb.GetItemInput
+	gotGetOutput *dynamodb.GetItemOutput
+	err          error
 }
 
-func (f *fakePutItemAPI) PutItem(_ context.Context, in *dynamodb.PutItemInput, _ ...func(*dynamodb.Options)) (*dynamodb.PutItemOutput, error) {
-	f.gotInput = in
+func (f *fakeDynamoAPI) PutItem(_ context.Context, in *dynamodb.PutItemInput, _ ...func(*dynamodb.Options)) (*dynamodb.PutItemOutput, error) {
+	f.gotPutInput = in
 	return &dynamodb.PutItemOutput{}, f.err
 }
 
-func TestSave_MarshalsAndCallsPutItem(t *testing.T) {
-	fp := &fakePutItemAPI{}
+func (f *fakeDynamoAPI) GetItemByKey(ctx context.Context, in *dynamodb.GetItemInput, _ ...func(*dynamodb.Options)) (*dynamodb.GetItemOutput, error) {
+	f.gotGetInput = in
+	return f.gotGetOutput, f.err
+}
+
+func TestAddShortURL_MarshalsAndCallsPutItem(t *testing.T) {
+	fp := &fakeDynamoAPI{}
 	s := New(fp, "test-table")
-	err := s.Save(context.Background(), &models.UrlData{
+	err := s.AddShortURL(context.Background(), &models.UrlData{
 		FullURL:   "https://www.google.com",
 		ShortURL:  "eswp9Xga",
 		CreatedAt: time.Now(),
 	})
 	require.NoError(t, err)
-	require.Equal(t, "test-table", *fp.gotInput.TableName)
-	require.Contains(t, fp.gotInput.Item, "full_url")
-	require.Contains(t, fp.gotInput.Item, "short_url")
+	require.Equal(t, "test-table", *fp.gotPutInput.TableName)
+	require.Contains(t, fp.gotPutInput.Item, "full_url")
+	require.Contains(t, fp.gotPutInput.Item, "short_url")
 }
 
-func TestSave_PropagatesError(t *testing.T) {
-	s := New(&fakePutItemAPI{err: errors.New("boom")}, "t")
-	err := s.Save(context.Background(), &models.UrlData{})
+func TestAddShortURL_PropagatesError(t *testing.T) {
+	s := New(&fakeDynamoAPI{err: errors.New("boom")}, "t")
+	err := s.AddShortURL(context.Background(), &models.UrlData{})
+	require.ErrorContains(t, err, "boom")
+}
+
+func TestGetFullURL_Found(t *testing.T) {
+	item, err := attributevalue.MarshalMap(&models.UrlData{
+		ShortURL: "eswp9Xga",
+		FullURL:  "https://www.google.com",
+	})
+	require.NoError(t, err)
+
+	fp := &fakeDynamoAPI{gotGetOutput: &dynamodb.GetItemOutput{Item: item}}
+	s := New(fp, "test-table")
+
+	got, err := s.GetFullURL(context.Background(), "eswp9Xga")
+	require.NoError(t, err)
+	require.Equal(t, "https://www.google.com", got.FullURL)
+	require.Equal(t, "eswp9Xga", got.ShortURL)
+
+	// the right key was sent to dynamo
+	require.Equal(t, "test-table", *fp.gotGetInput.TableName)
+	require.Contains(t, fp.gotGetInput.Key, "short_url")
+}
+
+func TestGetFullURL_NotFound(t *testing.T) {
+	// Item == nil means dynamo found no matching key.
+	fp := &fakeDynamoAPI{gotGetOutput: &dynamodb.GetItemOutput{}}
+	s := New(fp, "test-table")
+
+	_, err := s.GetFullURL(context.Background(), "missing")
+	require.ErrorIs(t, err, ErrNotFound)
+}
+
+func TestGetFullURL_PropagatesError(t *testing.T) {
+	s := New(&fakeDynamoAPI{err: errors.New("boom")}, "t")
+	_, err := s.GetFullURL(context.Background(), "x")
 	require.ErrorContains(t, err, "boom")
 }
